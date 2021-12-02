6cc63bb380b8f468248a4ffe4a72b791637ca6cd
Yan Coquoz <Yan.Coquoz@gmail.com>
2021-12-02 17:33:49 +0100

    Base de l'app_meteo

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Meteo />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Meteo from "./components/Meteo.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Meteo,
+  },
+};
 </script>
 
 <style>
