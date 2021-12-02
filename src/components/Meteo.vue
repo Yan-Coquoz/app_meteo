154046f7e9c1d82a2ccc00a622d4a40ad8c94f91
Yan Coquoz <Yan.Coquoz@gmail.com>
2021-12-02 20:50:35 +0100

    Mise en place du template / bootstrap

--- src/components/Meteo.vue
+++ src/components/Meteo.vue
@@ -1,13 +1,30 @@
 <template>
-  <h1>Hello my App</h1>
+  <div class="container">
+    <h1 class="my-4">La météo du ciel</h1>
+    <div class="form-groupe mb-5">
+      <label for="position" class="mb-3">Entrez le nom d'une ville</label>
+      <input
+        id="position"
+        type="text"
+        class="form-control"
+        v-model="requete"
+        v-on:keypress="getMeteo"
+      />
+    </div>
+    <div class="w-75 m-auto">
+      <h3 class="text-center mb-4">Position : localité</h3>
+      <div class="card text-center p-5 rad-15">
+        <p class="text-affichage">Température : les degrés</p>
+        <p class="texte-affichage">Temps : visuel</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Meteo",
-  props: {
-    msg: String,
-  },
+  props: {},
 };
 </script>
 
